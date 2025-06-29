c09db45d34257edf2b29da6d76be1d22d79fb60b
Marius0101 <mariusoctavian010@gmail.com>
2025-06-29 22:01:22 +0300

    add authorize endpoint

---
+++ OpenIDConnect.Auth/Pages/connect/authorize/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+
+namespace OpenIDConnect.Auth.Pages.connect.authorize;
+public class AuthorizeModel : PageModel
+{
+    public async Task<IActionResult> OnGetAsync()
+    {
+
+        if (!User.Identity.IsAuthenticated)
+        {
+            return Redirect($"/Account/Login{Request.QueryString}");
+        }
+        return await OnPostAcceptAsync();
+    }
+    
+    public async Task<IActionResult> OnPostAcceptAsync()
+    {
+        var request = HttpContext.GetOpenIddictServerRequest();
+
+        var claims = new List<Claim>
+        {
+            new Claim(OpenIddictConstants.Claims.Subject, User.FindFirstValue(ClaimTypes.NameIdentifier) ?? ""),
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims,
+            OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+        claimsPrincipal.SetScopes(request.GetScopes());
+        
+        return SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+    }
+}
--- OpenIDConnect.Auth/Seeding/ClientSeeder.cs
+++ OpenIDConnect.Auth/Seeding/ClientSeeder.cs
@@ -7,18 +7,19 @@ public class ClientSeeder
     public static async Task SeedAsync(IServiceProvider serviceProvider)
     {
         var applicationManager  = serviceProvider.GetRequiredService<IOpenIddictApplicationManager>();
-        if (await applicationManager.FindByClientIdAsync("api") == null)
+        if (await applicationManager.FindByClientIdAsync("my-client") == null)
         {
             await applicationManager.CreateAsync(new OpenIddictApplicationDescriptor
             {
                 ClientId = "my-client",
                 ClientSecret = "secret",
+                RedirectUris = { new Uri("https://localhost:4200/callback") },
                 DisplayName = "My Client App",
                 Permissions =
                 {
-                    OpenIddictConstants.Permissions.Endpoints.Token,
-                    OpenIddictConstants.Permissions.GrantTypes.ClientCredentials,
-                    OpenIddictConstants.Permissions.Scopes.Profile,
+                    OpenIddictConstants.Permissions.Endpoints.Authorization,
+                    OpenIddictConstants.Permissions.GrantTypes.AuthorizationCode,
+                    OpenIddictConstants.Permissions.ResponseTypes.Code,
                     "api"
                 }
             });
