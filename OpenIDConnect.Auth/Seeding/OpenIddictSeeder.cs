2ef3a3fe5355af079483c9bacfe40a58fa01799a
Marius0101 <mariusoctavian010@gmail.com>
2025-06-29 22:26:40 +0300

    add user seedre

--- OpenIDConnect.Auth/Seeding/OpenIddictSeeder.cs
+++ OpenIDConnect.Auth/Seeding/OpenIddictSeeder.cs
@@ -5,8 +5,9 @@ public class OpenIddictSeeder
     public static async Task SeedAsync(IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
-        
+
         await ClientSeeder.SeedAsync(scope.ServiceProvider);
         await ScopeSeeder.SeedAsync(scope.ServiceProvider);
+        await UserSeeder.SeedAsync(scope.ServiceProvider);
     }
 }
