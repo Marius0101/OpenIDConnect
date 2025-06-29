347e28b6c68f1ccc68284efc2a2e3c3bcd064b26
Marius0101 <mariusoctavian010@gmail.com>
2025-06-29 22:01:00 +0300

    add razor

--- OpenIDConnect.Auth/Program.cs
+++ OpenIDConnect.Auth/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OpenIDConnect.Auth.Seeding;
 using OpenIDConnect.Infrastructure.DbContex;
+using OpenIddict.Server;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,14 +11,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-    
+
     options.UseOpenIddict();
 });
+
+builder.Services.AddRazorPages();
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
-    
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000);
+
+    options.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.UseHttps();
+    });
+});
+
 builder.Services.AddOpenIddict()
     .AddCore(options =>
     {
@@ -57,6 +70,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.MapRazorPages();
 
 app.UseHttpsRedirection();
 
